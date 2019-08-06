eb13f4cdb2bb9ea3058abf4ef145830ca8e647cb
yangjie <yangjie@deepdraw.cn>
2019-08-06 15:49:41 +0800

    d

--- karma-pre-push.conf.js
+++ karma-pre-push.conf.js
@@ -12,11 +12,11 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/PROJECT_NAME'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
+    // coverageIstanbulReporter: {
+    //   dir: require('path').join(__dirname, './coverage/PROJECT_NAME'),
+    //   reports: ['html', 'lcovonly', 'text-summary'],
+    //   fixWebpackSourcePaths: true
+    // },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
