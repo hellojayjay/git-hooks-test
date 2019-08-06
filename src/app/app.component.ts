819c8396ec61fe4212c32293fde8b84d3ddc695c
yangjie <yangjie@deepdraw.cn>
2019-08-06 15:14:20 +0800

    测试build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment as env } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'git-hooks-test';
+  flag = env.notInProd;
 }
